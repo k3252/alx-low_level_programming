a9437129b4667a41f4a9c1e00cc946b3d3695e4c
k3252 <kirubelayele123@gmail.com>
2022-12-19 05:59:22 -0800

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * strepy - copiar el contendido de uno aotra variable
+ * @dest: this is destiny
+ * @src: this is copi
+ *
+ * Return: this return
+ */
+char *_strepy(char *dest, char *src)
+{
+	char *start = dest;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src;
+	}
+	*dest = '\0';
+	return (start);
+}
