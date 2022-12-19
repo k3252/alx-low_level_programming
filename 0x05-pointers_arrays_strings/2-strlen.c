84d7573cc306f528901d857b4e2b5e31b7c65e45
k3252 <kirubelayele123@gmail.com>
2022-12-19 00:26:21 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - function thar returns the length of string
+ *
+ * @s: this is the input string
+ *
+ * Return: lenngth of string
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' ; index++)
+
+		return(index);
+}
