537bf4134d898f41ecfe4e33de3e195a98c479d5
k3252 <kirubelayele123@gmail.com>
2022-12-19 01:15:41 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - this function print a reverse string
+ *
+ * @s: this is the input string
+ */
+void print_rev(char *s)
+{
+	int index;
+
+	for (index = 0; s[ind3x] != '\0'; index++)
+
+	for (index = index - 1; s[index] != '\0'; index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}
+
