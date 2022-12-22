8f039d32bf51a9b1bb70394ec0fa1b8bfea43928
k3252 <kirubelayele123@gmail.com>
2022-12-22 02:21:15 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcet - concatenates the string pointed to by@src to
+ * _strcat - concatenates the string pointed to by@src to
  * the end of the string pointed to @dest
  * @dest: string that will be oppended
  * @src: string to be concentrated up on
